493832cb07a5ecb0b47d6e6c2708280d7e0dd5aa
greatstudentkaze <dfyz0r@list.ru>
2019-11-20 19:21:14 +0300

    Парсер открывается в окне

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import requests
 from bs4 import BeautifulSoup as bs
+from tkinter import *
 
 headers = {
     'accept': '*/*',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36'
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/78.0.3904.97 Safari/537.36 '
 }
 
 base_url = 'https://yandex.ru/pogoda/saint-petersburg/details?via=ms'
@@ -27,18 +29,13 @@ def weather_parse():
         fc_dates = []
         for i in range(len(fc_day)):
             a, b, c = fc_day[i], fc_month[i], fc_weekday[i]
-            fc_date = f'{a.text} {b.text} ({c.text})'
+            fc_date = f'{a.text} {b.text}\n({c.text})'
             fc_dates.append(fc_date)
-
-            # тестовый вывод
-            # print(fc_fd)
-        # print(len(fc_dates))
 
         # достаем список температуры утром, днем, вечером, ночью на 10 дней
         fc_temp_fdt = fc_det.find_all('div', attrs={'class': 'weather-table__temp'})
         fc_temps_ad = []
         fc_temps = {}
-        # k = 0
         for i in range(40):
             d = fc_temp_fdt[i]
 
@@ -52,24 +49,40 @@ def weather_parse():
                 fc_temps.update({'night': d.text})
                 fc_temps_ad.append(fc_temps)
                 fc_temps = {}
-                # k += 1
 
-        # тестовый вывод
-        # print(len(fc_temps_ad))
-        # # print(len(fc_temps_ad))
-        # for t in fc_temps_ad:
-        #     print(t)
-        # for i in fc_temps:
-        #     print(fc_temps[i])
         for i in range(10):
-            output = f'{fc_dates[i]} \t- утром: {fc_temps_ad[i]["morning"]}\n\t\t\t\t' \
-                     f'\t\t- днем: {fc_temps_ad[i]["day"]}\n\t\t\t\t' \
-                     f'\t\t- вечером: {fc_temps_ad[i]["evening"]}\n\t\t\t\t' \
-                     f'\t\t- ночью: {fc_temps_ad[i]["night"]}\n\t'
-            print(output)
+            weekday_example = 'Утро\nДень\nВечер\nНочь'
+            weekday = f'{fc_temps_ad[i]["morning"]}\n{fc_temps_ad[i]["day"]}\n{fc_temps_ad[i]["evening"]}\n' \
+                      f'{fc_temps_ad[i]["night"]}'
+
+            weekday_label = Label(root, text=weekday_example, font=("Arial", 15), borderwidth=2, relief="groove")
+            weekday_label.grid(row=2, column=0, padx=15)
+            Label(root, borderwidth=2, relief="groove", text=fc_dates[i], fg="#fff", bg="#222",
+                  font=("Arial", 15), width=12).grid(row=1, column=i + 1, pady=10)
+            Label(root, borderwidth=2, relief="groove", text=weekday, font=("Arial", 15),
+                  width=12).grid(row=2, column=i + 1)
+
+            # консольный вывод
+            # output = f'{fc_dates[i]} \t- утром: {fc_temps_ad[i]["morning"]}\n\t\t\t\t' \
+            #          f'\t\t- днем: {fc_temps_ad[i]["day"]}\n\t\t\t\t' \
+            #          f'\t\t- вечером: {fc_temps_ad[i]["evening"]}\n\t\t\t\t' \
+            #          f'\t\t- ночью: {fc_temps_ad[i]["night"]}\n\t'
+            # print(output)
 
     else:
-        print('ERROR')
+        # print('ERROR')
+        Label(root, text='ERROR', bg="red", fg="#fff").grid(row=1, column=1, columnspan=2)
 
 
-weather_parse()
+root = Tk()
+root.title('Парсер сайта Яндекс Погода')
+root.geometry("1500x250")
+
+updateButton = Button(root, text="Update", fg="#fff", bg="#0d801b", font=("Arial", 15), relief="flat",
+                      command=weather_parse)
+updateButton.grid(row=3, columnspan=2, column=0, pady=15, padx=20)
+
+copyLabel = Label(root, text='GSK © 2019', font=("Arial", 15))
+copyLabel.grid(row=3, columnspan=2, column=10, pady=15)
+
+root.mainloop()
